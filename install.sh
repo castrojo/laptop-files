773fc83cdf07ed026430b91264a59eb9aef9c3f2
Jorge O. Castro <jorge.castro@gmail.com>
2018-04-18 21:04:45 -0400

    Add root check

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@
 ##
 # Based on
 # https://unix.stackexchange.com/questions/310361/packages-to-be-install-in-a-for-loop-if-not-installed
-
 set -e
+[ "$UID" -eq 0 ] || { echo "This script must be run as root."; exit 1;} # Need to figure out how to pkexec so we only ask for the password once.
 
 REQPKGS=(chromium communitheme darktable discord docker fast firefox gimp gitkraken gmusic gnome-3-26-1604 gnome-calculator google-play-music-desktop-player google-webapp gotop htop hugo inkscape irssi libreoffice lxd mailspring musescore mutt obs-studio sdlpop signal-desktop snappy-debug spotify vidcutter vlc youtube-dl-casept)
 
