cb6b619514d4e90e50a3d7c1afceef3b0652381f
Jorge O. Castro <jorgec@vmware.com>
2020-04-10 16:42:13 -0400

    Yay zoom!
    Signed-off-by: Jorge O. Castro <jorgec@vmware.com>

--- install.sh
+++ install.sh
@@ -7,11 +7,10 @@
 set -e
 [ "$UID" -eq 0 ] || { echo "This script must be run as root."; exit 1;} # Need to figure out how to pkexec so we only ask for the password once.
 
-snap install chromium darktable discord docker fast firefox gimp htop hugo inkscape libreoffice lxd mailspring obs-studio signal-desktop vlc youtube-dl-casept krita youtube-music-desktop-app bluemail
+snap install chromium darktable discord docker fast firefox gimp htop hugo inkscape libreoffice lxd obs-studio signal-desktop vlc youtube-dl krita youtube-music-desktop-app prospect-mail zoom-client
 
 CLASSICREQPKGS=(blender code restic node go kubectl slack skype)
 
 for pkg in "${CLASSICREQPKGS[@]}"; do
         snap install "$pkg" --classic && echo "Successfully installed $pkg"
 done
-
