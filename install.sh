99f7faad20070559d00a73338b5a2f5be30ea8f2
Jorge O. Castro <jorgec@vmware.com>
2020-04-10 16:52:22 -0400

    Add debs too

--- install.sh
+++ install.sh
@@ -7,10 +7,15 @@
 set -e
 [ "$UID" -eq 0 ] || { echo "This script must be run as root."; exit 1;} # Need to figure out how to pkexec so we only ask for the password once.
 
-snap install chromium darktable discord docker fast firefox gimp htop hugo inkscape libreoffice lxd obs-studio signal-desktop vlc youtube-dl krita youtube-music-desktop-app prospect-mail zoom-client
+snap install chromium darktable discord docker fast firefox gimp htop hugo inkscape libreoffice lxd obs-studio signal-desktop vlc youtube-dl krita youtube-music-desktop-app prospect-mail zoom-client barrier
 
 CLASSICREQPKGS=(blender code restic node go kubectl slack skype)
 
 for pkg in "${CLASSICREQPKGS[@]}"; do
         snap install "$pkg" --classic && echo "Successfully installed $pkg"
 done
+
+sudo apt update 
+sudo apt install -y git zsh flameshot build-essential steam wireguard-dkms zfsutils-linux flatpak network-manager-openconnect-gnome variety
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
