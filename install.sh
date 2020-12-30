f4e3ff132212b16d5e800669543b7177dd896a89
Jorge O. Castro <jorge.castro@gmail.com>
2020-12-29 19:04:04 -0500

    Add sound switcher indicator
    Signed-off-by: Jorge O. Castro <jorge.castro@gmail.com>

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@
 set -e
 [ "$UID" -eq 0 ] || { echo "This script must be run as root."; exit 1;} # Need to figure out how to pkexec so we only ask for the password once.
 
-snap install chromium discord docker fast flameshot glimpse-editor htop inkscape libreoffice obs-studio signal-desktop vlc youtube-dl krita prospect-mail zoom-client hey-mail
+snap install chromium discord fast flameshot glimpse-editor htop inkscape libreoffice obs-studio signal-desktop vlc youtube-dl krita prospect-mail zoom-client hey-mail sound-switcher-indicator
 
-snap install 1password --beta
+snap install 1password --edge
 
 CLASSICREQPKGS=(blender code restic node go kubectl slack skype)
 
@@ -18,6 +18,6 @@ for pkg in "${CLASSICREQPKGS[@]}"; do
 done
 
 sudo apt update 
-sudo apt install -y git zsh build-essential steam zfsutils-linux flatpak network-manager-openconnect-gnome variety
+sudo apt install -y git zsh build-essential steam zfsutils-linux flatpak variety
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
