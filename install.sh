0bb9a16304d6ba066b7b45f1cc782d33ffbfac30
Jorge O. Castro <jorge.castro@arrikto.com>
2021-06-07 11:51:58 -0400

    Update apps
    Signed-off-by: Jorge O. Castro <jorge.castro@arrikto.com>

--- install.sh
+++ install.sh
@@ -7,17 +7,12 @@
 set -e
 [ "$UID" -eq 0 ] || { echo "This script must be run as root."; exit 1;} # Need to figure out how to pkexec so we only ask for the password once.
 
-snap install chromium discord fast flameshot glimpse-editor htop inkscape libreoffice obs-studio signal-desktop vlc youtube-dl krita prospect-mail zoom-client hey-mail indicator-sound-switcher
-
-snap install 1password --edge
+snap install chromium discord fast flameshot htop inkscape libreoffice obs-studio signal-desktop vlc youtube-dl krita zoom-client indicator-sound-switcher mailspring standard-notes starship todoist youtube-music-desktop-app 1password triangle-wallpaper
 
 CLASSICREQPKGS=(blender code restic node go kubectl slack skype)
 
 for pkg in "${CLASSICREQPKGS[@]}"; do
         snap install "$pkg" --classic && echo "Successfully installed $pkg"
 done
-
-sudo apt update 
-sudo apt install -y git zsh build-essential steam zfsutils-linux flatpak variety
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
